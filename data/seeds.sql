e7eba276c6299c2588f191799649bb8183c1b0d8
Laura <lhernan53112@gmail.com>
2020-05-27 22:48:16 -0400

    Updated table columns. Added a inner join to displyed roles table with department column included

--- data/seeds.sql
+++ data/seeds.sql
@@ -1,19 +1,31 @@
 -- Seeds for employeeMgmtSys_DB
 USE employeeMgmtSys_DB;
 
--- Eployee values
-INSERT INTO employee (first_name,last_name,role_id,manager_id)
-VALUES 
-("Ana", "cruz",4,5);
+-- department values
+INSERT INTO department (name)
+VALUES ("sales"),("Engineering"),("Finance"),("Legal");
 
 -- role values
 INSERT INTO role (title,salary,department_id)
 VALUES 
-("sales", 80000,4);
+("Sales Lead", 40000,1),
+("Sales person", 35000,1),
+("Lead Engineering", 90000,2),
+("Accountant", 70000, 3),
+("lawyer", 50000, 4);
 
--- department values
-INSERT INTO department (name)
+-- Eployee values
+INSERT INTO employee (first_name,last_name,role_id,manager_id)
 VALUES 
-("sales");
+("Ana", "cruz"),
+("marisol", "lopez"),
+("maria", "martinez"),
+("juana", "paulino");
 
 SELECT * FROM department;
+SELECT * FROM role;
+
+-- All Roles left join
+SELECT role.id, title,name salary 
+FROM department
+INNER JOIN role ON role.department_id=department.id;
